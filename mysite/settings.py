48686c2eb88d230450eab6a93e4a0ca07f5f0797
JungYoung <apollyon4@naver.com>
2019-12-30 11:01:41 +0900

    Allowed host add

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(fpo+vjf=zw7utuyci0haz1szu^0$3)uogmdox^1a0r_c&2c6l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.pythonanywhere.com']
 
 
 # Application definition
